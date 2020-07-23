ada1a6e99bbbc1e8913ec9d9cf7be05a643c831e
Adam C <adam@deniapps.com>
2020-07-22 21:12:11 -0400

    added upload plugin back. The integration is needed.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,9 @@
 # Changelog
 
-7/7/2020 - v1.0.0 - Initial version with ckeditor5-build-classic v20.0.0
+- 07/22/2020 - v1.0.4 - add ImageUpload plugin
 
-1. removed ckfinder, easyimage
-2. added codeblock & insert image by url
+- 07/07/2020 - v1.0.0 - Initial version with ckeditor5-build-classic v20.0.0
+  - removed ckfinder, easyimage
+  - added codeblock & insert image by url
 
 All changes in the package are documented in the main repository. See: https://github.com/ckeditor/ckeditor5/blob/master/CHANGELOG.md.
--- src/InsertImage.js
+++ src/InsertImage.js
@@ -1,36 +1,39 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import ButtonView from "@ckeditor/ckeditor5-ui/src/button/buttonview";
+import imageIcon from "@ckeditor/ckeditor5-core/theme/icons/image.svg";
 
 export default class InsertImage extends Plugin {
-    init() {
-        const editor = this.editor;
+  init() {
+    const editor = this.editor;
 
-        editor.ui.componentFactory.add( 'insertImage', locale => {
-            const view = new ButtonView( locale );
+    editor.ui.componentFactory.add("insertImage", locale => {
+      const view = new ButtonView(locale);
 
-            view.set( {
-                label: 'Insert image',
-                icon: imageIcon,
-                tooltip: true
-            } );
+      view.set({
+        label: "Insert image url",
+        icon: imageIcon,
+        tooltip: true
+      });
 
-            // Callback executed once the image is clicked.
-            view.on( 'execute', () => {
-                const imageUrl = prompt( 'Image URL' );
-								if(!imageUrl) return false;
+      // Callback executed once the image is clicked.
+      view.on("execute", () => {
+        const imageUrl = prompt("Image URL");
+        if (!imageUrl) return false;
 
-                editor.model.change( writer => {
-                    const imageElement = writer.createElement( 'image', {
-                        src: imageUrl
-                    } );
+        editor.model.change(writer => {
+          const imageElement = writer.createElement("image", {
+            src: imageUrl
+          });
 
-                    // Insert the image in the current selection location.
-                    editor.model.insertContent( imageElement, editor.model.document.selection );
-                } );
-            } );
+          // Insert the image in the current selection location.
+          editor.model.insertContent(
+            imageElement,
+            editor.model.document.selection
+          );
+        });
+      });
 
-            return view;
-        } );
-    }
+      return view;
+    });
+  }
 }
