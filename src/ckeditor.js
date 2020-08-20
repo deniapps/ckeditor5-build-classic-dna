991cd5fc02b67c6e958493b4b918c21043ba600e
Adam C <adam@deniapps.com>
2020-08-19 20:23:52 -0400

    added upsplash

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -27,6 +27,7 @@ import Table from "@ckeditor/ckeditor5-table/src/table";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
 import InsertImage from "./InsertImage";
+import InsertImageFromUnsplash from "./InsertImageFromUnsplash";
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -45,6 +46,7 @@ ClassicEditor.builtinPlugins = [
   ImageToolbar,
   ImageUpload,
   InsertImage,
+  InsertImageFromUnsplash,
   Indent,
   Link,
   List,
@@ -52,7 +54,7 @@ ClassicEditor.builtinPlugins = [
   Paragraph,
   Table,
   TableToolbar,
-  TextTransformation
+  TextTransformation,
 ];
 
 // Editor configuration.
@@ -71,20 +73,26 @@ ClassicEditor.defaultConfig = {
       "outdent",
       "|",
       "insertImage",
+      "insertImageFromUnsplash",
       "codeBlock",
       "blockQuote",
       "insertTable",
       "mediaEmbed",
       "undo",
-      "redo"
-    ]
+      "redo",
+    ],
   },
   image: {
-    toolbar: ["imageStyle:full", "imageStyle:side", "|", "imageTextAlternative"]
+    toolbar: [
+      "imageStyle:full",
+      "imageStyle:side",
+      "|",
+      "imageTextAlternative",
+    ],
   },
   table: {
-    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
   },
   // This value must be kept in sync with the language defined in webpack.config.js.
-  language: "en"
+  language: "en",
 };
